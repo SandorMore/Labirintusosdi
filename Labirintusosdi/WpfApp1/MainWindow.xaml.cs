a6ce2c25d1488ae84335ddb37464d163c26ea070
SandorMore <msaso0725@gmail.com>
2026-05-17 15:14:35 +0200

    added stuff not yet ready

--- Labirintusosdi/WpfApp1/MainWindow.xaml.cs
+++ Labirintusosdi/WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Win32;
+using System.CodeDom;
 using System.IO;
+using System.Numerics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,9 +21,18 @@ namespace WpfApp1
     {
         Labyrinth labyrinth;
         LANG lang = LANG.HUN;
+        Rectangle player;
+
+        int playerX = 1;
+        int playerY = 1;
+        const int TILESIZE = 20;
         public MainWindow()
         {
             InitializeComponent();
+
+
+            lbRead.Content = (lang == LANG.HUN) ? "Fájl beolvasása" : "Read file";
+            btnRead.Content = (lang == LANG.HUN) ? "Olvasás" : "Read";
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -29,10 +40,13 @@ namespace WpfApp1
             var res = read_map();
             if (res != null)
             {
-                Tuple<char[,], int, int> t = res;
-                labyrinth = new Labyrinth(t.Item2, t.Item3, t.Item1);
+                labyrinth = new Labyrinth(res.Item2, res.Item3, res.Item1);
             }
             labyrinth.print_labyrinth();
+            if(res != null) 
+                render_map(res.Item1, res.Item2, res.Item3);
+            
+            create_player();
         }
         Tuple<char[,], int, int>? read_map()
         {
@@ -84,5 +98,57 @@ namespace WpfApp1
 
             return Tuple.Create(map, width, height);
         }
+        void render_map(char[,] map, int width, int height)
+        {
+            gameCanvas.Children.Clear();
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    sb.Append(map[y, x]);
+                }
+
+                sb.AppendLine();
+            }
+
+            TextBlock tb = new TextBlock();
+
+            tb.Text = sb.ToString();
+
+            tb.FontFamily = new FontFamily("Consolas");
+
+
+            if (width <= 25 && height <= 25)
+                tb.FontSize = 45;
+            else if (width <= 40 && height <= 40)
+                tb.FontSize = 30;
+            else
+                tb.FontSize = 20;
+
+            tb.Foreground = Brushes.White;
+
+            Canvas.SetLeft(tb, 0);
+            Canvas.SetTop(tb, 0);
+
+            gameCanvas.Children.Add(tb);
+        }
+        void create_player()
+        {
+            player = new Rectangle();
+
+            player.Width = TILESIZE;
+            player.Height = TILESIZE;
+
+            player.Fill = Brushes.Green;
+
+            Canvas.SetLeft(player, playerX * TILESIZE);
+            Canvas.SetTop(player, playerY * TILESIZE);
+
+            gameCanvas.Children.Add(player);
+        }
+
     }
 }
